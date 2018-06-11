fd012617f075100fdd0da0bb52dbf826eb38912a
Jason Cihelka <jason.cihelka@jcace.com>
2018-06-11 17:09:05 -0600

    Add TokenTimelock contract

---
+++ contracts/TokenTimelock.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.24;
+
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+/**
+ * @title TokenTimelock
+ * @dev TokenTimelock is a token holder contract that will allow a
+ * beneficiary to extract the tokens after a given release time
+ */
+contract TokenTimelock {
+
+  // ERC20 basic token contract being held
+  StandardToken public token;
+
+  // beneficiary of tokens after they are released
+  address public beneficiary;
+
+  // timestamp when token release is enabled
+  uint256 public releaseTime;
+
+  function TokenTimelock(StandardToken _token, address _beneficiary, uint256 _releaseTime) public {
+    // solium-disable-next-line security/no-block-members
+    require(_releaseTime > block.timestamp);
+    token = _token;
+    beneficiary = _beneficiary;
+    releaseTime = _releaseTime;
+  }
+
+  /**
+   * @notice Transfers tokens held by timelock to beneficiary.
+   */
+  function release() public {
+    // solium-disable-next-line security/no-block-members
+    require(block.timestamp >= releaseTime);
+
+    uint256 amount = token.balanceOf(this);
+    require(amount > 0);
+
+    token.transfer(beneficiary, amount);
+  }
+}

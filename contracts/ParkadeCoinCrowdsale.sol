a34b16e422a1e66589950ec13d102d36cca49524
Jason Cihelka <jason.cihelka@jcace.com>
2018-07-02 15:17:11 -0600

    Fix unusedTokensWithdrawalTime

--- contracts/ParkadeCoinCrowdsale.sol
+++ contracts/ParkadeCoinCrowdsale.sol
@@ -29,7 +29,7 @@ contract ParkadeCoinCrowdsale is TimedCrowdsale, RefundableCrowdsale, Whiteliste
 
   // Timestamp indicating when unsold tokens may be withdrawn by the Parkade.io wallet for future use
   // Sept 14, 2019 12:00:00AM GMT
-  uint256 public unusedTokensWithdrawalTime = 1536883200;
+  uint256 public unusedTokensWithdrawalTime = 1568419200;
 
   // A separate Ethereum address which only has the right to add addresses to the whitelist
   // It is not permitted to access any other functionality, or to claim funds
